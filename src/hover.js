c4d76b0d2951c923385fc2a1428c48231b1adcc5
Charles Buxton <chazbux@gmail.com>
2020-03-11 15:44:37 -0600

    finished text styling animation

--- src/hover.js
+++ src/hover.js
@@ -3,11 +3,51 @@ import React from 'react'
 class Hover extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      x: 0,
+      y: 0
+    }
+  }
+
+  movement = (e) => {
+    //get width of div that holds decorated text
+    const width = this.refs.titleContainer.clientWidth
+    const height = this.refs.titleContainer.clientHeight
+    //store x and y offset variables from mouse movements
+    const offX = Math.floor((e.nativeEvent.offsetX / width) * 100)
+    const offY = Math.floor((e.nativeEvent.offsetY / height) * 100)
+    // console.log(offX, offY)
+    this.setState({
+      x: offX,
+      y: offY
+    })
   }
 
   render() {
+    const { x, y } = this.state
+    const styles = {
+      container: {
+        position: 'relative'
+      },
+      light: {
+        color: '#2D4571',
+        fontSize: '90px',
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        clipPath: `polygon(0 0, ${x}% 0%, ${y}% 100%, 0% 100%)`,
+        transition: 'all 0.8s cubic-bezier(0.165,0.84,0.44,1'
+      },
+      dark: {
+        color: '#7688A9',
+        fontSize: '90px'
+      },
+    }
+    
     return (
-      <div className='titleContainer' style={{...styles.container}}>
+      <div ref='titleContainer'
+        style={{ ...styles.container }}
+        onMouseMove={this.movement}>
         <div style={{ ...styles.light }}>
           <h1>{this.props.text}</h1>
         </div>
@@ -18,32 +58,7 @@ class Hover extends React.Component {
     )
   }
 
-
-}
-
-let xPos = 0;
-let yPos = 60;
-
-const styles = {
-  container: {
-    position: 'relative'
-  },
-  light: //cloneWrapper
-   {
-    color: '#2D4571',
-    fontSize: '90px',
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    clipPath: `polygon(0 0, ${xPos}% 100%, ${yPos}% 100%)`
-
-  },
-  dark: //titleWrapper
-  {
-    color: '#7688A9',
-    fontSize: '90px',
-   
-  },
+ 
 
 }
 
